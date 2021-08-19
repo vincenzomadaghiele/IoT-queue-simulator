8f92a27e2deb2cf50cd61ddc51c1b54746d15f79
Tommaso Carluccio <tommasocarluccio96@gmail.com>
2021-08-19 11:35:10 +0200

    task1a

--- Lab2/Task1.py
+++ Lab2/Task1.py
@@ -18,17 +18,19 @@ if __name__ == '__main__':
     ARRIVAL = SERVICE/LOAD
 
     # SYSTEM PARAMS 
-    BUFFER_SIZE = 5 #float('inf')
+    BUFFER_SIZE = 25 #float('inf')
     FOG_NODES = 1 # number of fog nodes
     f=0.7
     SERVICE_CLOUD=50
-    CLOUD_BUFFER_SIZE = 20
+    CLOUD_BUFFER_SIZE = 100
     CLOUD_SERVERS= 1
     
     # SIMULATION PARAMS
-    SIM_TIME = 300000
+    SIM_TIME = 500000
     X=[]
-    num_sim=1
+    X_transient = []
+    num_sim=50
+    xj=[]
     for seed in range(num_sim):
         random.seed(seed)
         np.random.seed(seed)
@@ -42,21 +44,45 @@ if __name__ == '__main__':
         print_everything = False
         data, data_cloud, time, _, _ = s.simulate(print_everything)
 
+        n = len(data_cloud.waitingDelay)
         x = np.mean(data_cloud.waitingDelay)
+
+        xj.append(data_cloud.waitingDelay)
+
+        x = np.mean(data_cloud.waitingDelay)
+        x_tr = np.mean(data_cloud.waitingDelay[250:])
         X.append(x)
+        X_transient.append(x_tr)
         # cumulate statistics
         #print(data_cloud.waitingDelay)
     X_mean=np.mean(X)
     S=np.std(X)
-    print(f'Average delay = {X_mean} +- {2.326 * S / np.sqrt(num_sim)} (confidence 98%)')
-       
-    n = len(data_cloud.waitingDelay)
-    x = np.mean(data_cloud.waitingDelay)
+    X_mean_tr=np.mean(X_transient)
+    S_tr=np.std(X_transient)
+    print(f'Average delay = {X_mean} +- {3.090 * S / np.sqrt(num_sim)} (confidence 99.8%)')
+    print(f'Average delay (without transient)= {X_mean_tr} +- {3.090 * S_tr / np.sqrt(num_sim)} (confidence 99.8%)')
     
+    random.seed(42)
+    np.random.seed(42)
+    
+    # data storage object
+    data = sim.Measure(0,0,0,0,0,0,0,0,0,0,[],[],0)
+    data_cloud = sim.Measure(0,0,0,0,0,0,0,0,0,0,[],[],0)
+
+    # simulator
+    s = sim.Simulator(data, data_cloud, LOAD, SERVICE, ARRIVAL, BUFFER_SIZE, FOG_NODES, SIM_TIME, f, CLOUD_SERVERS, CLOUD_BUFFER_SIZE, SERVICE_CLOUD)
+    print_everything = False
+    data, data_cloud, time, _, _ = s.simulate(print_everything)
+
+    XJ = np.zeros(n)
+    for xx in xj:
+        XJ += np.array(xx)
+    XJ /= num_sim
+
     xk_list = []
     Rk = []
     for k in range(n):
-        xk = np.mean(data_cloud.waitingDelay[k:])
+        xk = np.mean(XJ[k:])
         xk_list.append(xk)
         Rk.append((xk - x) / x)
 
@@ -88,6 +114,7 @@ if __name__ == '__main__':
     plt.legend()
     plt.xlabel("Packets")
     plt.ylabel("xk")
+    #plt.ylim([0,3000])
     plt.show()
 
     plt.plot(Rk, label='Rk')
